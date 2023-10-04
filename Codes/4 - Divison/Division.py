51b4038943e14a335e05a59f60c0323ff27478c0
guilbezerra <guilherme.bezerra@ciel-it.com>
2023-10-04 10:28:38 -0300

    Divisão aproximada e inteira funcionando

---
+++ Codes/4 - Divison/Division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    print("Este programa dividirá e mostrará o valor aproximado e inteiro da divisão dos numeros A e B que forem passados.")
+    a = int(input("Digite o numero A: "))
+    b = int(input("Digite o numero B: "))
+
+    print((a//b), (a/b), sep='\n')
