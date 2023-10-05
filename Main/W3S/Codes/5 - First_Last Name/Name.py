8edfda3fec3edf0aee2e22ae25a10b321ad89076
Guil-Hanma <guilherme.lima170@hotmail.com>
2023-10-05 09:07:44 -0300

    Só pra incluir algo mesmo

--- Main/W3S/Codes/5 - First_Last Name/Name.py
+++ Main/W3S/Codes/5 - First_Last Name/Name.py
@@ -1,3 +1,5 @@
 fname = str(input("Qual seu primeiro nome? ").strip())
 lname = str(input("Qual seu sobrenome? ").strip())
+# Comentario desnecessario
 print("Seu nome é: ", lname, fname)
+print("to testando")
