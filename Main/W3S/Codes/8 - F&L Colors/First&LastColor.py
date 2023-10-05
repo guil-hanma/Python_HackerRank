762fe179eba8988bbf3db908d829739d7cb51029
Guil-Hanma <guilherme.lima170@hotmail.com>
2023-10-05 14:45:59 -0300

    Teste2

--- Main/W3S/Codes/8 - F&L Colors/First&LastColor.py
+++ Main/W3S/Codes/8 - F&L Colors/First&LastColor.py
@@ -1,6 +1,7 @@
 # 8. Write a Python program to display the first and last colors from the following list.
 # color_list = ["Red","Green","White" ,"Black"]
 print("teste")
+print("teste2")
 
 color_list = ["Red", "Green", "White", "Black"]
 print(f"A primeira cor é: {color_list[0]} \nE a ultima cor é: {color_list[3]}")
