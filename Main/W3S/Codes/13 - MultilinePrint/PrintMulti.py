0cd7c7749a9c295fae5b0c3e22c6343d3a8587c3
Guil-Hanma <guilherme.lima170@hotmail.com>
2023-10-06 14:11:59 -0300

    Multiline print utilizando """ """

---
+++ Main/W3S/Codes/13 - MultilinePrint/PrintMulti.py
@@ -0,0 +1,7 @@
+print("""
+a string that you "don't" have to escape                                                                      
+This                                                                                                          
+is a  ....... multi-line                                                                                      
+heredoc string --------> example 
+
+      """)
